299c78d08d3d12b6c696086597674f12beb7d1aa
K S Daana Fathima <fathimadaana14@gmail.com>
2025-06-12 07:18:35 +0530

    Update App.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,9 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './components/LoginPage';
-import StudentMarkPage from './components/StudentMarkPage';
-import StudentNotePage from './components/StudentNotePage';
-import StudentHomePage from './components/StudentHomePage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import LoginPage from "./components/LoginPage";
+import StudentHomePage from "./components/StudentHomePage";
+import StudentMarkPage from "./components/StudentMarkPage";
+import StudentNotePage from "./components/StudentNotePage";
+
+import HomePage from "./components/HomePage";
+import UploadMarks from "./components/UploadMarks";
+import AddNotes from "./components/AddNotes";
 function App() {
   return (
     <Router>
@@ -12,9 +17,13 @@ function App() {
         <Route path="/student" element={<StudentHomePage />} />
         <Route path="/student-marks" element={<StudentMarkPage />} />
         <Route path="/student-notes" element={<StudentNotePage />} />
-        </Routes>
+        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/uploadMarks" element={<UploadMarks />} />
+        <Route path="/addNotes" element={<AddNotes />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
