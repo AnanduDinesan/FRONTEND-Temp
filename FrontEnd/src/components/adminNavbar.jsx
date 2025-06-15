e8386e29a25a4bddca2fe3f7438e0e776dfe4970
ADHULJITH <athuljith333@gmail.com>
2025-06-16 05:17:59 +0530

    admin-design

--- FrontEnd/src/components/adminNavbar.jsx
+++ FrontEnd/src/components/adminNavbar.jsx
@@ -4,23 +4,24 @@ import '../styling/adminNavbar.css';
 
 const Navbar = () => {
   return (
-    <> {/* Use a React Fragment to return multiple top-level elements */}
-      {/* Top Horizontal Navigation Bar */}
+    <> 
       <nav className="top-navbar">
         <ul className="top-nav-list">
           <li><Link to="/" className="top-nav-button">Home</Link></li>
           <li><Link to="/enquire" className="top-nav-button">Enquire</Link></li>
           <li><Link to="/helps" className="top-nav-button">Helps</Link></li>
         </ul>
+        <div className="logout-button-container">
+          <Link to="/" className="logout-button">Logout</Link>
+        </div>
       </nav>
 
-      {/* Left Vertical Dashboard Navigation */}
       <nav className="dashboard-navbar">
         <ul className="dashboard-nav-list">
           <li><Link to="/department" className="dashboard-nav-button">Department</Link></li>
           <li><Link to="/user" className="dashboard-nav-button">User</Link></li>
           <li><Link to="/subject" className="dashboard-nav-button">Subject</Link></li>
-          <li><Link to="/showdetails" className="dashboard-nav-button">show details</Link></li>
+          <li><Link to="/showdetails" className="dashboard-nav-button">Show Details</Link></li>
         </ul>
       </nav>
     </>
