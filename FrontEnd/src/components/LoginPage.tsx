a227d639f174f3a33a7fdd6e6002dbb264151eeb
ArjunBabu2810 <arjunvellaikudam@gmail.com>
2025-06-24 09:52:18 +0530

    nothing

--- FrontEnd/src/components/LoginPage.tsx
+++ FrontEnd/src/components/LoginPage.tsx
@@ -3,12 +3,12 @@ import { useNavigate } from 'react-router-dom';
 import api from '../api';
 import '../styling/LoginPage.css';
 
-interface LoginDetails{
+interface LoginDetails {
   email: string;
   password: string;
 }
 
-interface ResponseStruct{
+interface ResponseStruct {
   token: string;
   id: string;
   name: string;
@@ -26,13 +26,14 @@ const LoginPage: React.FC = () => {
     email: '',
     password: ''
   });
-  const handleChange= (e:React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setFormData(prev => ({
       ...prev,
       [name]: value
-    }))};
-  const handleLogin = async (e:React.FormEvent<HTMLFormElement>) => {
+    }))
+  };
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     setError('');
     setLoading(true);
@@ -40,7 +41,7 @@ const LoginPage: React.FC = () => {
     try {
       const response = await api.post<ResponseStruct>('auth/login', formData);
       console.log(response.data);
-      const { role, name, id, departmentId, email} = response.data;
+      const { role, name, id, departmentId, email } = response.data;
 
       localStorage.setItem('jwt', response.data.token);
       localStorage.setItem('user', JSON.stringify({ id, name, role, email, departmentId }));
@@ -49,7 +50,7 @@ const LoginPage: React.FC = () => {
       else if (role === 'teacher') navigate('/teacher');
       else if (role === 'student') navigate('/student');
       else setError('Invalid user role.');
-    } catch (err:any) {
+    } catch (err: any) {
       console.log(err);
       setError('Invalid email or password.');
     } finally {
@@ -83,6 +84,53 @@ const LoginPage: React.FC = () => {
         </button>
       </form>
     </div>
+    // <div className="flex items-center">
+
+    //   <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+    //     <form
+    //       onSubmit={handleLogin}
+    //       className="bg-white shadow-lg rounded-xl p-6 w-full max-w-sm"
+    //       >
+    //       <h2 className="text-2xl font-bold mb-6 text-center text-gray-800">
+    //         Login
+    //       </h2>
+
+    //       <input
+    //         type="email"
+    //         placeholder="Email"
+    //         name="email"
+    //         value={formData.email}
+    //         onChange={handleChange}
+    //         required
+    //         className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+    //         />
+
+    //       <input
+    //         type="password"
+    //         placeholder="Password"
+    //         name="password"
+    //         value={formData.password}
+    //         onChange={handleChange}
+    //         required
+    //         className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+    //         />
+
+    //       {error && (
+    //         <p className="text-red-500 text-sm mb-4">{error}</p>
+    //       )}
+
+    //       <button
+    //         type="submit"
+    //         disabled={loading}
+    //         className="w-full bg-blue-600 text-white py-2 rounded-md hover:bg-blue-700 transition disabled:opacity-50"
+    //         >
+    //         {loading ? 'Loading...' : 'Login'}
+    //       </button>
+    //     </form>
+    //   </div>
+    // </div>
+
+
   );
 };
 
